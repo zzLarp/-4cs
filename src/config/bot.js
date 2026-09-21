dee42d7dd6adf60803a16b969d1dd93e93c3f0d7
Larp <zzlarpzz@gmail.com>
2026-09-21 16:38:25 +0300

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
       {
         name: "/4cs", // required by Discord API, not shown in the client
         state: "/4cs",     // this is what people actually see
-        type: 2,               // Custom
+        type: 1,               // Custom
       },
     ],
   },
